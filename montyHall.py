339601694e0166cf36285ab9ab37433fc801845a
astorlucas <lucasastor@gmail.com>
2023-03-24 19:10:49 -0300

    condicional

---
+++ montyHall.py
@@ -0,0 +1,8 @@
+print("hola mundo")
+
+test = True
+
+if test:
+    print("hola 1")
+else:
+    print("hola 2")
---
+++ probCondicional.py
@@ -0,0 +1,15 @@
+def prob_A(A,O):
+    prob_A=A/O
+    return prob_A()
+
+if __name__=="__main__":
+    A=3
+    O=6
+
+    prob_A= A/O
+
+    print(f"Y este sera nuestro resultado: {prob_A}")
+
+
+
+
